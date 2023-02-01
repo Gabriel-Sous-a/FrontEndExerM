e12ca4ca7af1e2c54073dbbe506350d25840e223
Gabriel Sousa <gabriel2001@live.com.pt>
2023-02-01 19:11:31 +0000

    Update: to do list

--- Local Storage ex/script.js
+++ Local Storage ex/script.js
@@ -4,52 +4,131 @@ const input = document.getElementById("value");
 const remove = document.getElementById("delete");
 const localRead = JSON.parse(localStorage.getItem("list"));
 const father = document.getElementById("div");
+const description = document.getElementById("description");
 let list = document.getElementById("list");
 let counter = 0;
 let ul;
 let storage = [];
+let id;
 
-document.getElementById("form").addEventListener("submit", (e) => {
+document.getElementById("menu").addEventListener("submit", (e) => {
   e.preventDefault();
 });
-
 
 if (localRead) {
   storage = localRead;
   for (let i = 0; i < storage.length; i++) {
     const li = document.createElement("li");
-    li.innerHTML = storage[i].input;
     li.setAttribute("id", storage[i].id);
+    const div = document.createElement("div");
+    div.innerHTML = storage[i].description;
+    li.innerHTML = storage[i].input;
+    li.appendChild(div);
     list.appendChild(li);
-
-    const buttonEdit = document.createElement("button");
-    buttonEdit.innerHTML = "Edit";
-    buttonEdit.setAttribute("class", "position");
-    li.appendChild(buttonEdit);
-
-    const button = document.createElement("button");
-    button.innerHTML = "X";
-    button.setAttribute("class", "position");
-    li.appendChild(button);
-    removeItems(button);
-    input.value = "";
+    addButtons(li);
   }
 }
 
+value.addEventListener("click", () => {
+  console.log("--");
+  const div = document.createElement("div");
+  const description = document.createElement("input");
+  const date = document.createElement("input");
+});
+
 add.addEventListener("click", () => {
-  if (input.value === "") {
-    return;
-  }
   const li = document.createElement("li");
-  li.innerHTML = input.value;
+  if (input.value === "") {
+    li.innerHTML = "Untitled";
+  } else {
+    li.innerHTML = input.value;
+  }
+  const div = document.createElement("div");
+  div.innerHTML = description.value;
+  li.appendChild(div);
   li.setAttribute("id", counter);
   list.appendChild(li);
   storage.push({
     input: input.value,
     id: counter,
+    description: description.value,
   });
   localStorage.setItem("list", JSON.stringify(storage));
+  addButtons(li);
+  input.value = "";
+  description.value = "";
+});
 
+remove.addEventListener("click", () => {
+  list.remove();
+  ul = document.createElement("ul");
+  ul.setAttribute("id", "list");
+  father.appendChild(ul);
+  storage = [];
+  list = ul;
+  localStorage.clear();
+});
+
+function edit(buttonEdit, li) {
+  const editInput = document.createElement("input");
+  const editDescription = document.createElement("input");
+  editDescription.setAttribute("type", "textarea");
+  const confirm = document.createElement("button");
+  confirm.innerHTML = "ok";
+  confirm.setAttribute("type", "submit");
+  for (let i = 0; i < storage.length; i++) {
+    if (storage[i].id == buttonEdit.parentElement.id) {
+      confirm.setAttribute("id", storage[i].id);
+    }
+  }
+  buttonEdit.addEventListener("click", () => {
+    li.innerHTML = "";
+    li.appendChild(editInput);
+    li.appendChild(editInput);
+    li.appendChild(editDescription);
+    li.appendChild(confirm);
+    confirm.addEventListener("click", () => {
+      for (let i = 0; i < storage.length; i++) {
+        if (storage[i].id == confirm.id) {
+          storage.splice(i, 1, {
+            input: editInput.value,
+            id: counter,
+            description: editDescription.value,
+          });
+          counter++;
+        }
+      }
+      console.log(storage);
+      const inputValue = editInput.value;
+      const inputDescription = editDescription.value;
+      const div = document.createElement("div");
+      li.innerHTML = "";
+      li.innerHTML = inputValue;
+      div.innerHTML = inputDescription;
+      li.appendChild(div);
+      addButtons(li);
+      input.value = "";
+      localStorage.setItem("list", JSON.stringify(storage));
+    });
+  });
+}
+
+function removeItems(button) {
+  button.addEventListener("click", () => {
+    for (let i = 0; i < storage.length; i++) {
+      if (storage[i].id == button.parentElement.id) {
+        storage.splice(i, 1);
+      }
+    }
+    localStorage.setItem("list", JSON.stringify(storage));
+    button.parentElement.remove();
+    if (storage.length == 0) {
+      localStorage.clear();
+    }
+  });
+}
+
+function addButtons(li) {
   const buttonEdit = document.createElement("button");
   buttonEdit.innerHTML = "Edit";
   buttonEdit.setAttribute("class", "position");
@@ -62,66 +141,4 @@ add.addEventListener("click", () => {
   li.appendChild(button);
   counter++;
   removeItems(button);
-
-  input.value = "";
-});
-
-function edit(buttonEdit, li) {
-  const editInput = document.createElement("input");
-  const confirm = document.createElement("button");
-  confirm.innerHTML = "ok";
-  confirm.setAttribute("type", "submit");
-  let inputValue = "";
-  buttonEdit.addEventListener("click", () => {
-    console.log(li);
-    li.innerHTML = "";
-    li.appendChild(editInput);
-    li.appendChild(confirm);
-    confirm.addEventListener("click", () => {
-      inputValue = editInput.value;
-      li.innerHTML = "";
-      li.innerHTML = inputValue;
-
-      const buttonEdit = document.createElement("button");
-      buttonEdit.innerHTML = "Edit";
-      buttonEdit.setAttribute("class", "position");
-      li.appendChild(buttonEdit);
-      edit(buttonEdit, li);
-
-      const button = document.createElement("button");
-      button.innerHTML = "X";
-      button.setAttribute("class", "position");
-      li.appendChild(button);
-      counter++;
-      removeItems(button);
-      input.value = "";
-    });
-  });
 }
-
-function removeItems(button) {
-  button.addEventListener("click", () => {
-    for (let i = 0; i < storage.length; i++) {
-      if (storage[i].id == button.parentElement.id) {
-        storage.splice(i, 1);
-        console.log("in");
-      }
-    }
-    localStorage.setItem("list", storage);
-    console.log(storage);
-    button.parentElement.remove();
-    if (storage.length == 0) {
-      localStorage.clear();
-    }
-  });
-}
-
-remove.addEventListener("click", () => {
-  list.remove();
-  ul = document.createElement("ul");
-  ul.setAttribute("id", "list");
-  father.appendChild(ul);
-  storage = [];
-  list = ul;
-  localStorage.clear();
-});
